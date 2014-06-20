5d01785a25c3d45ce3ea81b59806543e6122b673
Jim Rustle <jim_rustle@live.com>
2014-06-20 18:19:51 -0400

    Make Matlab implementation into FFT, not signal plot

--- fft/Matlab/ffft.m
+++ fft/Matlab/ffft.m
@@ -2,7 +2,10 @@
 fid = fopen("/tmp/mpd.fifo", 'rb');
 while 1
     vals = fread(fid, 1024, 'int16')/256 + 256;
-    plot(vals);
+    %plot(vals);
+    ys = abs(fft(vals)(3:128));
+    plot(ys/sum(ys));
+    axis([0, 128-3, 0, 0.2]);
     drawnow;
 end
 fclose(fid);
