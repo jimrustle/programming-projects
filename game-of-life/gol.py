7861c4c590b28283b3224b660d9b29fdf4d9e29c
Jim Rustle <jim_rustle@live.com>
2014-07-15 18:37:21 -0400

    Remove magic numbers from Game of Life

--- game-of-life/gol.py
+++ game-of-life/gol.py
@@ -1,34 +1,36 @@
+SIZE = 20
+
 def shiftup(array):
     k = list(array)
-    old = k[:7]
-    return k[7:]+old
+    old = k[:SIZE]
+    return k[SIZE:]+old
 
 def shiftdown(array):
     k = list(array)
-    old = k[-7:]
-    return old + k[:42]
+    old = k[-SIZE:]
+    return old + k[:SIZE*(SIZE-1)]
 
 def shiftleft(array):
-    k = [0]*49
-    old = array[0:49:7]
-    for i in range(0, 48):
+    k = [0]*SIZE**2
+    old = array[0:SIZE**2:SIZE]
+    for i in range(0, SIZE**2 - 1):
             k[i] = array[i+1]
-    for i in range(6, 49, 7):
-        k[i] = old[i/7]
+    for i in range(SIZE-1, SIZE**2, SIZE):
+        k[i] = old[i/SIZE]
     return k
 
 def shiftright(array):
-    k = [0]*49
-    old = array[6:49:7]
-    for i in range(1, 49):
+    k = [0]*SIZE**2
+    old = array[SIZE-1:SIZE**2:SIZE]
+    for i in range(1, SIZE**2):
             k[i] = array[i-1]
-    for i in range(0, 49, 7):
-        k[i] = old[i/7]
+    for i in range(0, SIZE**2, SIZE):
+        k[i] = old[i/SIZE]
     return k
 
 def add(a, b, c, d, e, f, g, h):
-    k = [0]*49
-    for i in range(49):
+    k = [0]*SIZE**2
+    for i in range(SIZE**2):
         k[i] = a[i] + b[i] + c[i] + d[i] \
                + e[i] + f[i] + g[i] + h[i]
     return k
@@ -42,24 +44,24 @@ def next_state(current):
                        shiftup(shiftright(current)), \
                        shiftdown(shiftleft(current)), \
                        shiftdown(shiftright(current)))
-    return [1 if ((current[i] == 1 and k[i] == 2) or k[i] == 3) else 0 for i in range(49)]
+    return [1 if ((current[i] == 1 and k[i] == 2) or k[i] == 3) else 0 for i in range(SIZE**2)]
 
 def print_array(a):
-    for i in range(7):
-        for j in range(7):
-            print a[7*i+j],
+    for i in range(SIZE):
+        for j in range(SIZE):
+            print a[SIZE*i+j],
         print
     print "---"
 
 lifearray = []
 
-for i in range(1, 8):
-    for j in range(7):
+for i in range(SIZE):
+    for j in range(SIZE):
         lifearray.append(0)
 
 lifearray[5] = 1
-lifearray[12]= 1
-lifearray[19] = 1
+lifearray[25]= 1
+lifearray[45] = 1
 
 #lifearray[9] = 1
 #lifearray[10] = 1
