44119dc0353e2af3eb836511e7e56260221dce3f
Jim Rustle <jim_rustle@live.com>
2014-11-17 01:16:07 -0500

    Remove C macro, replace with cast
    Cast is neater in this situation

--- chip-8/C/chip8.c
+++ chip-8/C/chip8.c
@@ -1,24 +1,28 @@
 #include <stdio.h>
 #include <stdint.h>
 
-#define CONV_BYTES_TO_I16(A, B) ((unsigned char) ((unsigned char) (A) << 8 | (unsigned char) (B)))
-
 int main(int argc, char *argv[]) {
-    int upper, lower;
+    char upper, lower;
     int16_t opcode;
     FILE * file;
+
     if (argc != 2) {
         printf("Wrong number of arguments -- should be one\n");
         return 1;
     }
 
     file = fopen(argv[1], "r");
+
     if (!file) {
         printf("Error opening %s\n", argv[1]);
         return 1;
     }
-    while ((upper = fgetc(file), lower = fgetc(file)) != EOF) {
-        opcode = CONV_BYTES_TO_I16(upper, lower);
+
+    while ((upper = (char) fgetc(file),
+            lower = (char) fgetc(file)) != EOF) {
+
+        opcode = (unsigned char) (upper << 8 | lower);
+
         if (opcode == 0x00e0) {
             printf("Clear screen!\n");
         }
